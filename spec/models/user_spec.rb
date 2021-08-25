394c64688816cc64a9d796bc618d8ccb07a617af
Jacob Piland <piland.jacob@gmail.com>
2021-08-25 18:52:10 -0500

    Add validation tests to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe User, type: :model do
     it { should have_many(:events) }
     it { should have_many(:attendees) }
   end
+  describe 'Validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
 end
