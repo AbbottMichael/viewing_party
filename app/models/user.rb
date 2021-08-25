dece5772ab8f42bd3fa185a920f28f9bff3f46ca
Jacob Piland <piland.jacob@gmail.com>
2021-08-25 18:55:12 -0500

    Add validations to user model to pass spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   has_many :followed, through: :followed_users
   has_many :following_users, foreign_key: :follower_id , class_name: "Friend"
   has_many :followers, through: :following_users
+
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, require: true
+
+  has_secure_password 
 end
