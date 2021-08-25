fd219c3ffe3cae4a36a9d90bf95e02192de582fc
Michael Abbott <michael@mjabbottdesign.com>
2021-08-24 18:56:56 -0700

    Add description and link to register user tests

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -9,13 +9,19 @@ RSpec.describe 'Welcome page' do
     expect(page).to have_content('Welcome to Viewing Party!')
   end
 
-  it 'displays a brief discription of the app'
+  it 'displays a brief discription of the app' do
+    description = 'This is an application to explore movies and create a viewing party event for you and your friends to watch a movie together.'
+    expect(page).to have_content(description)
+  end
+
+  it 'has a link to register a new user' do
+    click_on('Register a new user')
+    expect(current_path).to eq(new_user_path)
+  end
 
   describe 'log in' do
     it 'can log in with valid credentials'
 
     it 'can not log in with invalid credentials'
   end
-
-  it 'has a link to register a new user'
 end
