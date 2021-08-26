a3cf54ae83b7648c649baf21e8ef02160b04173b
Jacob Piland <piland.jacob@gmail.com>
2021-08-25 20:28:21 -0500

    Add handrolled routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 
 root 'welcome#index'
 
-resources :users, only: [:new]
+# resources :users, only: [:new]
 
 post '/', to: 'users#login'
 
 get '/dashboard', to: 'dashboard#index'
+
+get '/register', to: 'users#new'
+post '/register', to: 'users#create'
 end
