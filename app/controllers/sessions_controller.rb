d0592016be8892dc9bef53c9db29b3d61267f1d2
Michael Abbott <michael@mjabbottdesign.com>
2021-08-26 15:43:13 -0700

    Refactor to remove log in flash message on Welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
       redirect_to root_path
     elsif user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.email}!"
       redirect_to dashboard_path
     else
       flash[:error] = "Sorry, your credentials are bad."
