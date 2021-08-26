7a06ce3bb5e29d07a8fb626f0eccf1eb7f951f9e
Jacob Piland <piland.jacob@gmail.com>
2021-08-25 20:42:04 -0500

    Refactor create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
 
   def new
-    user = User.new
+    @user = User.new
   end
 
   def create
-    user = user_params
-    user[:email] = user[:email].downcase
-    new_user = User.create(user)
-    flash[:success] = "Welcome, #{new_user.email}!"
-    redirect_to root_path
+    if params[:user][:password] == params[:user][:password_confirmation]
+      user = user_params
+      user[:email] = user[:email].downcase
+      new_user = User.create(user)
+      flash[:success] = "Welcome, #{new_user.email}!"
+      redirect_to dashboard_path
+    else
+      flash[:error] = "Passwords do not match."
+      redirect_to register_path
+    end
   end
 
   def login
@@ -28,6 +33,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
