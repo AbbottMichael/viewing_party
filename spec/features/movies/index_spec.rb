2d8693be45e2667b4125f30ce349a2c1e4ad6441
Michael Abbott <michael@mjabbottdesign.com>
2021-08-28 16:51:58 -0700

    Add feature test discover index; update feature test movies index

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -6,18 +6,36 @@ RSpec.describe 'The Movie Index page' do
       visit movies_path
 
       expect(page.status_code).to eq(200)
-      expect(page).to have_content("Dilwale Dulhania Le Jayenge")
-      expect(page).to have_content(8.8)
-      # expect(@popular_movies_1[keys]['title'].count).to eq(20)
-      # expect(@popular_movies_2.count).to eq(20)
+      within '#Top-Movies' do
+        expect(page).to have_content("Dilwale Dulhania Le Jayenge")
+        expect(page).to have_content(8.8)
+        expect(page).to have_css("p", count: 40)
+      end
     end
 
     it "links to an individual movie's show page", :vcr do
       visit movies_path
-      
+
       click_on "Dilwale Dulhania Le Jayenge"
 
       expect(current_path).to eq(movie_path(19404))
+    end
+
+    it "displays a search form", :vcr do
+      visit movies_path
+
+      fill_in 'search', with: 'batman'
+      click_on 'Find Movies'
+
+      expect(current_path).to eq(movies_path)
+
+      within '#Search' do
+        expect(page).to have_content('Batman')
+        expect(page).to have_content('Batman Begins')
+        expect(page).to have_content('The Lego Batman Movie')
+        expect(page).to_not have_content("Dilwale Dulhania Le Jayenge")
+        expect(page).to have_css("p", maximum: 40)
+      end
     end
   end
 end
