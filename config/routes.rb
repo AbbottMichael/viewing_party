9d9b2fc67d359968b068bb1ebdc7a1a792e3687f
Michael Abbott <michael@mjabbottdesign.com>
2021-08-31 16:46:54 -0700

    Remove commented out data

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,24 @@ Rails.application.routes.draw do
 
 root 'welcome#index'
 
-# resources :users, only: [:new]
-
 post '/', to: 'sessions#create'
 
 get '/dashboard', to: 'dashboard#index'
+
 get '/logout', to: 'sessions#destroy'
 
 get '/discover', to: 'discover#index'
 
 post '/friends', to: 'friends#create'
 
-get '/register', to: 'users#new'
+get  '/register',  to: 'users#new'
 post '/register', to: 'users#create'
 
-# get '/movies', to: 'movies#index'
 resources :movies, only: [:index, :show]
 
 post '/movies', to: 'movies#index'
 
-get '/viewing-party/new', to: 'events#new'
+get  '/viewing-party/new',  to: 'events#new'
 post '/viewing-party/new', to: 'events#create'
 
 end
