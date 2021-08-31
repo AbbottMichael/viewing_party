0b1536e606af488c709884baedc2d92abbba3d63
Michael Abbott <michael@mjabbottdesign.com>
2021-08-31 16:51:15 -0700

    Add Friend model #find_email test

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe Friend, type: :model do
     it { should belong_to(:follower) }
     it { should belong_to(:followed) }
   end
+
+  before :each do
+    @user = User.create(
+      email: 'funbucket@gmail.com',
+      password: 'password1',
+      password_confirmation: 'password1'
+    )
+    @user1 = User.create(
+      email: 'funbucket1@gmail.com',
+      password: 'password1',
+      password_confirmation: 'password1'
+    )
+    @friend1 = Friend.create!(follower_id: @user1.id, followed_id: @user.id)
+  end
+
+  describe 'instance methods' do
+    describe '#find_email' do
+      it "can find the email of the friend" do
+        expect(@friend1.find_email).to eq(@user1.email)
+      end
+    end
+  end
 end
