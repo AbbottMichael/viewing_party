60b812de0fcf09dd197c7d9d83a03832b897ab3a
Michael Abbott <michael@mjabbottdesign.com>
2021-08-31 16:51:45 -0700

    Add Event model #find_host_email

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,4 +5,31 @@ RSpec.describe Event, type: :model do
     it { should have_many(:attendees) }
     it { should belong_to(:user) }
   end
+  before :each do
+    @user = User.create(
+      email: 'funbucket@gmail.com',
+      password: 'password1',
+      password_confirmation: 'password1'
+    )
+    @user1 = User.create(
+      email: 'funbucket1@gmail.com',
+      password: 'password1',
+      password_confirmation: 'password1'
+    )
+    @attended_event = Event.create!(
+      host_id: @user1.id,
+      movie_id: 680,
+      title: 'Pulp Fiction',
+      viewing_time: 154,
+      date_time: "2024-09-01T08:30"
+    )
+  end
+
+  describe 'instance methods' do
+    describe '#find_host_email' do
+      it "can find the email of the event host" do
+        expect(@attended_event.find_host_email).to eq(@user1.email)
+      end
+    end
+  end
 end
