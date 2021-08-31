0a7c8bdb974c486447ba87b02c6859762b59ae81
Michael Abbott <michael@mjabbottdesign.com>
2021-08-31 16:56:20 -0700

    Add Attendee model #find_email, #find_event_by_id tests

--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -5,4 +5,40 @@ RSpec.describe Attendee, type: :model do
     it { should belong_to(:event) }
     it { should belong_to(:user) }
   end
+
+  before :each do
+    @user = User.create(
+      email: 'funbucket@gmail.com',
+      password: 'password1',
+      password_confirmation: 'password1'
+    )
+    @user1 = User.create(
+      email: 'funbucket1@gmail.com',
+      password: 'password1',
+      password_confirmation: 'password1'
+    )
+    @attended_event = Event.create!(
+      host_id: @user1.id,
+      movie_id: 680,
+      title: 'Pulp Fiction',
+      viewing_time: 154,
+      date_time: "2024-09-01T08:30"
+    )
+    @attendee = Attendee.create!(event_id: @attended_event.id, user_id: @user.id)
+
+  end
+
+  describe 'instance methods' do
+    describe '#find_email' do
+      it 'can find attendee email by user id' do
+        expect(@attendee.find_email).to eq(@user.email)
+      end
+    end
+
+    describe '#find_event_by_id' do
+      it 'can find an event by attendee id' do
+        expect(@attendee.find_event_by_id).to eq(@attended_event)
+      end
+    end
+  end
 end
