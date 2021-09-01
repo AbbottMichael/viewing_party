fa5121ede9c38a95e3d09db0c50ae4aa97e11420
Jacob Piland <piland.jacob@gmail.com>
2021-09-01 13:03:25 -0500

    Add authorized? method and remove commented methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,16 @@
 class ApplicationController < ActionController::Base
-  # before_action :popular_movies_1_fetch
-  # before_action :popular_movies_2_fetch
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :authorized? 
 
   def current_user
-      User.find(session[:user_id]) if session[:user_id]
+    User.find(session[:user_id]) if session[:user_id]
   end
 
-  # def popular_movies_1_fetch
-  #   json = MovieService.new.popular_movies_1
-  #   @popular_movies_1 = json[:results].map do |movie|
-  #     Movie.new(movie)
-  #   end
-  # end
-  #
-  # def popular_movies_2_fetch
-  #   json = MovieService.new.popular_movies_2
-  #   @popular_movies_2 = json[:results].map do |movie|
-  #     Movie.new(movie)
-  #   end
-  # end
+  def authorized?
+    if current_user.nil?
+      flash[:error] = "Please Log-In."
+      redirect_to root_path
+    end
+  end
 end
