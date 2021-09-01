561a06f79e19bf3739cc82c3084342dcccac179d
Michael Abbott <michael@mjabbottdesign.com>
2021-08-31 17:01:48 -0700

    Add Attendee model #find_email, #finde_event_by_id

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,12 @@
 class Attendee < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  def find_email
+    User.find(self.user_id).email
+  end
+
+  def find_event_by_id
+    Event.find(self.event_id)
+  end
 end
