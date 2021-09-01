ed39209b2893990819ecb301199e36400f0cd8fa
Michael Abbott <michael@mjabbottdesign.com>
2021-08-31 17:03:17 -0700

    Add MovieDetail poro #get_genre_names helper

--- app/poros/movie_detail.rb
+++ app/poros/movie_detail.rb
@@ -8,12 +8,18 @@ class MovieDetail
   def initialize(data)
     @title = data[:title]
     @runtime = data[:runtime]
-    @genres = data[:genres]
+    @genres = get_genre_names(data[:genres])
     @overview = data[:overview]
     @vote_average = data[:vote_average]
   end
 
   def format_time
-    "#{self.runtime / 60}h #{self.runtime % 60}min"
+    "#{self.runtime / 60}hr #{self.runtime % 60}min"
+  end
+
+  private
+
+  def get_genre_names(data)
+    data.map { |d| d[:name] }
   end
 end
