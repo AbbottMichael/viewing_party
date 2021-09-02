3fc90d5316fec1a31f56abb06df6db30d3afce1e
Michael Abbott <michael@mjabbottdesign.com>
2021-09-01 17:42:50 -0700

    Refactor MovieService calls from instance to class method

--- app/facades/movie_detail_facade.rb
+++ app/facades/movie_detail_facade.rb
@@ -1,6 +1,8 @@
 class MovieDetailFacade
   def self.movie_details_fetch(params_id)
-    json = MovieService.new.get_movie_details(params_id)
+    json = MovieService.get_movie_details(params_id)
     MovieDetail.new(json)
   end
+
+
 end
--- app/service/movie_service.rb
+++ app/service/movie_service.rb
@@ -1,15 +1,15 @@
 class MovieService
-  def get_movie_recommendations(movie_id)
+  def self.get_movie_recommendations(movie_id)
     endpoint = "https://api.themoviedb.org/3/movie/#{movie_id}/recommendations?api_key=#{ENV['movie_api_key']}&language=en-US&page=1"
     get_data(endpoint)
   end
 
-  def get_movie_poster(movie_id)
+  def self.get_movie_poster(movie_id)
     endpoint = "https://api.themoviedb.org/3/movie/#{movie_id}/images?api_key=#{ENV['movie_api_key']}"
     get_data(endpoint)
   end
 
-  def get_movie_search(search_param)
+  def self.get_movie_search(search_param)
     endpoint = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_api_key']}&language=en-US&page=1&include_adult=false&query=#{search_param}"
     endpoint2 = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_api_key']}&language=en-US&page=2&include_adult=false&query=#{search_param}"
     page1 = get_data(endpoint)
@@ -17,22 +17,22 @@ class MovieService
     page1.merge(page2) { |_key, page1_v, page2_v| page1_v + page2_v }
   end
 
-  def get_movie_details(movie_id)
+  def self.get_movie_details(movie_id)
     endpoint = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['movie_api_key']}&language=en-US"
     get_data(endpoint)
   end
 
-  def get_movie_cast(movie_id)
+  def self.get_movie_cast(movie_id)
     endpoint = "https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=#{ENV['movie_api_key']}&language=en-US"
     get_data(endpoint)
   end
 
-  def get_movie_reviews(movie_id)
+  def self.get_movie_reviews(movie_id)
     endpoint = "https://api.themoviedb.org/3/movie/#{movie_id}/reviews?api_key=#{ENV['movie_api_key']}&language=en-US&page=1"
     get_data(endpoint)
   end
 
-  def get_top_rated_movies
+  def self.get_top_rated_movies
     endpoint = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&language=en-US&page=1"
     endpoint2 = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&language=en-US&page=2"
     page1 = get_data(endpoint)
@@ -40,7 +40,7 @@ class MovieService
     page1.merge(page2) { |_key, page1_v, page2_v| page1_v + page2_v }
   end
 
-  def get_data(endpoint)
+  def self.get_data(endpoint)
     response = Faraday.get(endpoint)
     JSON.parse(response.body, symbolize_names: true)
   end
