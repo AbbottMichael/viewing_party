ece8e0db4c1ed35deed980d32bb929eeccdbc762
Michael Abbott <michael@mjabbottdesign.com>
2021-09-01 17:40:07 -0700

    Add User model #friends test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,36 @@ RSpec.describe User, type: :model do
     it { should have_many(:events) }
     it { should have_many(:attendees) }
   end
+
   describe 'Validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
+  end
+
+  describe 'instance methods' do
+    describe '#friends?' do
+      it "can determine if the users are friends" do
+        @user = User.create(
+          email: 'funbucket@gmail.com',
+          password: 'password1',
+          password_confirmation: 'password1'
+        )
+        @user1 = User.create(
+          email: 'funbucket1@gmail.com',
+          password: 'password1',
+          password_confirmation: 'password1'
+        )
+        @user2 = User.create(
+          email: 'funbucket2@gmail.com',
+          password: 'password1',
+          password_confirmation: 'password1'
+        )
+        @friend1 = Friend.create!(follower_id: @user1.id, followed_id: @user.id)
+
+        expect(@user.friends?(@user1)).to eq(true)
+        expect(@user.friends?(@user2)).to eq(false)
+      end
+    end
   end
 end
